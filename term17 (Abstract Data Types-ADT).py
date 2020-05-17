4bb33baae1e6a8d46247507d4292a7285fa90329
ahammadshawki8 <ahammadshawki8@gmail.com>
2020-05-17 15:49:15 +0600

    17-5-20

---
+++ term17 (Abstract Data Types-ADT).py
@@ -0,0 +1,33 @@
+
+# Abstract Data Types (ADT)
+
+# Short info:
+
+# if we want to store a data in our machine we have to use a certain datatype.
+# datatypes can be both simple(primitive) and complex.
+# in programming, normally we have huge amount of data and we want to put them together.
+# we can combine them in abstract datatypes like list, stack, queue, map.
+# they called abstract because they are concepts which are found in almost every language.
+# in different language we have different implementation for this adt.
+
+
+# Detail info:
+
+# what is data structure?
+# In computer science, a data structure is a data organization, management and storage format
+# that enables efficient access and modification.
+
+# we can understand data structures in 2 methods:
+# mathematical/ logical/ abstract models/ views
+# implementation
+
+# What is ADT?
+# Adts are entitis that are definitions of data and operations but do not have implementation details.
+# it means we know what and how data we are going to storing, what methods and operations we can do on that data,
+# how will we fetch or access data from those data types.
+# but we haven't yet implemented it in any practical sense.
+
+# the reason we do not have any implementaion because every different language have different way to implement those data types.
+# for example, when we have to store data which have a name and values,
+# we can use structures in c, classes in java, dictionaries and classes in python.
+# it means the implementation view vary from language to language but the logical views are same.
